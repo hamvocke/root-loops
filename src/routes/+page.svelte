2246b48b68f82a71e197fba8b4bf8943a475f828
Ham Vocke <ham@hamvocke.com>
2024-02-25 15:56:39 +0100

    add basic style, layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Root Loops</title>
+	<meta name="description" content="A color scheme for the cereal developer" />
+</svelte:head>
+
+<h1>Root Loops</h1>
