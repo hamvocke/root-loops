fdba312b67a15b3ba769557974d4a6115c7fa237
Ham Vocke <ham@hamvocke.com>
2024-03-03 10:37:50 +0100

    add bowl, cereals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
-<script>
+<script lang="ts">
 	import Slider from './Slider.svelte';
 	import { milk, flavor, colors } from './stores';
+
+	type Cereal = {
+		name: string;
+		color: string;
+	};
+
+	let cereals: Cereal[] = [
+		{ name: 'base01', color: '#000' },
+		{ name: 'base02', color: '#111' },
+		{ name: 'base03', color: '#222' },
+		{ name: 'base04', color: '#333' },
+		{ name: 'base05', color: '#444' },
+		{ name: 'base06', color: '#555' },
+		{ name: 'base07', color: '#666' },
+		{ name: 'base08', color: '#777' }
+	];
 </script>
 
 <svelte:head>
@@ -12,6 +28,12 @@
 	<h1>Root Loops</h1>
 	<p class="caption">A color scheme generator for cereal lovers.</p>
 </header>
+
+<section class="bowl">
+	{#each cereals as cereal}
+		<div class="cereal" style="background: {cereal.color}"></div>
+	{/each}
+</section>
 
 <section class="sliders">
 	<Slider id="slider-milk" label="Milk" bind:value={$milk} />
@@ -42,5 +64,17 @@
 		font-size: 1.4rem;
 		font-weight: 300;
 		font-style: italic;
+	}
+
+	.bowl {
+		display: flex;
+		gap: 2rem;
+		margin-bottom: 3rem;
+	}
+
+	.cereal {
+		height: 128px;
+		width: 128px;
+		border-radius: 50%;
 	}
 </style>
