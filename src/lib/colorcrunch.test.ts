8c8711992601a9831b9bd819de5b4ba9ca7df3bd
Ham Vocke <ham@hamvocke.com>
2024-03-18 12:25:24 +0100

    distribute hue equally

--- src/lib/colorcrunch.test.ts
+++ src/lib/colorcrunch.test.ts
@@ -21,4 +21,17 @@ describe('generateColors', () => {
     expect(colors[2].l).toBe(67);
     expect(colors[3].l).toBe(100);
   });
+
+  it('generates colors with equal hue distance', () => {
+    let colors = generateColors(8);
+
+    expect(colors[0].h).toBe(0);
+    expect(colors[1].h).toBe(45);
+    expect(colors[2].h).toBe(90);
+    expect(colors[3].h).toBe(135);
+    expect(colors[4].h).toBe(180);
+    expect(colors[5].h).toBe(225);
+    expect(colors[6].h).toBe(270);
+    expect(colors[7].h).toBe(315);
+  });
 });
--- src/lib/colorcrunch.ts
+++ src/lib/colorcrunch.ts
@@ -2,14 +2,21 @@ import Color from "colorjs.io";
 
 export function generateColors(count: number): Color[] {
   const l = (i: number) => lightness(i, count);
+  const h = (i: number) => hue(i, count);
 
   return new Array(count)
     .fill(undefined)
-    .map((_color, i) => new Color("oklch", [l(i), 0, 0]));
+    .map((_color, i) => new Color("oklch", [l(i), 0, h(i)]));
 }
 
 function lightness(index: number, colorCount: number): number {
   const maxLightness = 100;
   const step = maxLightness / (colorCount - 1);
+  return Math.round(step * index);
+}
+
+function hue(index: number, colorCount: number): number {
+  const maxHue = 360
+  const step = maxHue / (colorCount);
   return Math.round(step * index);
 }
