118e9db92e4fd58617637df181676ee6b2d97c21
Ham Vocke <ham@hamvocke.com>
2024-03-18 09:43:45 +0100

    add colorcrunch test, remove dummy test

--- src/lib/colorcrunch.test.ts
+++ src/lib/colorcrunch.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-describe('sum test', () => {
+describe('generateColors', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
 	});
