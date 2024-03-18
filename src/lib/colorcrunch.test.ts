20dc51c24a48adcc63cc3fd05c55ceb066cefc23
Ham Vocke <ham@hamvocke.com>
2024-03-18 12:13:53 +0100

    distribute lightness equally

--- src/lib/colorcrunch.test.ts
+++ src/lib/colorcrunch.test.ts
@@ -10,6 +10,16 @@ describe('generateColors', () => {
 
   it('generates colors in oklch space', () => {
     let colors = generateColors(5);
-    expect(colors.every(c => c.space === Color.spaces.oklch));
+    expect(colors.every(c => c.space === Color.spaces.oklch)).toBeTruthy();
+  });
+
+  it('generates colors with equal distance', () => {
+    let colors = generateColors(4);
+
+    expect(colors[0].l).toBe(0);
+    expect(colors[1].l).toBe(33);
+    expect(colors[2].l).toBe(67);
+    expect(colors[3].l).toBe(100);
+
   });
 });
--- src/lib/colorcrunch.ts
+++ src/lib/colorcrunch.ts
@@ -1,7 +1,12 @@
 import Color from "colorjs.io";
 
 export function generateColors(count: number): Color[] {
-  const colors: Color[] = new Array(count);
-  colors.fill(new Color("oklch", [0, 0, 0]));
-  return colors;
+  const maxLightness = 100;
+  const lightnessStep = maxLightness / (count - 1);
+
+  const l = (index: number) => Math.round(lightnessStep * index);
+
+  return new Array(count)
+    .fill(undefined)
+    .map((_color, i) => new Color("oklch", [l(i), 0, 0]));
 }
