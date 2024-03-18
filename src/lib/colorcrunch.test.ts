bf6598e03bd49bf1ab96624c6bfca06de7ac1a1f
Ham Vocke <ham@hamvocke.com>
2024-03-18 12:21:33 +0100

    refactoring

--- src/lib/colorcrunch.test.ts
+++ src/lib/colorcrunch.test.ts
@@ -13,13 +13,12 @@ describe('generateColors', () => {
     expect(colors.every(c => c.space === Color.spaces.oklch)).toBeTruthy();
   });
 
-  it('generates colors with equal distance', () => {
+  it('generates colors with equal lightness distance', () => {
     let colors = generateColors(4);
 
     expect(colors[0].l).toBe(0);
     expect(colors[1].l).toBe(33);
     expect(colors[2].l).toBe(67);
     expect(colors[3].l).toBe(100);
-
   });
 });
--- src/lib/colorcrunch.ts
+++ src/lib/colorcrunch.ts
@@ -1,12 +1,15 @@
 import Color from "colorjs.io";
 
 export function generateColors(count: number): Color[] {
-  const maxLightness = 100;
-  const lightnessStep = maxLightness / (count - 1);
-
-  const l = (index: number) => Math.round(lightnessStep * index);
+  const l = (i: number) => lightness(i, count);
 
   return new Array(count)
     .fill(undefined)
     .map((_color, i) => new Color("oklch", [l(i), 0, 0]));
+}
+
+function lightness(index: number, colorCount: number): number {
+  const maxLightness = 100;
+  const step = maxLightness / (colorCount - 1);
+  return Math.round(step * index);
 }
