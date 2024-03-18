bd9ebb20b017b98788c43f61d3ff32f5246c17ff
Ham Vocke <ham@hamvocke.com>
2024-03-18 12:41:30 +0100

    'equal lightness distance' my ass

--- src/lib/colorcrunch.ts
+++ src/lib/colorcrunch.ts
@@ -1,18 +1,11 @@
 import Color from "colorjs.io";
 
 export function generateColors(count: number): Color[] {
-  const l = (i: number) => lightness(i, count);
   const h = (i: number) => hue(i, count);
 
   return new Array(count)
     .fill(undefined)
-    .map((_color, i) => new Color("oklch", [l(i), 0.2, h(i)]));
-}
-
-function lightness(index: number, colorCount: number): number {
-  const maxLightness = 100;
-  const step = maxLightness / (colorCount - 1);
-  return Math.round(step * index);
+    .map((_color, i) => new Color("oklch", [0.6, 0.2, h(i)]));
 }
 
 function hue(index: number, colorCount: number): number {
