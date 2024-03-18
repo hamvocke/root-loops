65f9fe789bfee16c07a462926c04d16f3054780d
Ham Vocke <ham@hamvocke.com>
2024-03-18 09:53:18 +0100

    ensure all colors are oklch

--- src/lib/colorcrunch.test.ts
+++ src/lib/colorcrunch.test.ts
@@ -1,9 +1,15 @@
 import { describe, it, expect } from 'vitest';
 import { generateColors } from './colorcrunch';
+import Color from 'colorjs.io';
 
 describe('generateColors', () => {
 	it('generates the right amount of colors', () => {
     let colors = generateColors(16);
 		expect(colors.length).toBe(16);
 	});
+
+  it('generates colors in oklch space', () => {
+    let colors = generateColors(5);
+    expect(colors.every(c => c.space === Color.spaces.oklch));
+  });
 });
