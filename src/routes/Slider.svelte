17a462c147b18a795cfe75a4def516264877a43c
Ham Vocke <ham@hamvocke.com>
2024-03-25 10:29:38 +0100

    separate slider value from label

--- src/routes/Slider.svelte
+++ src/routes/Slider.svelte
@@ -6,5 +6,21 @@
 
 <div class="slider">
 	<input type="range" {id} name={id} min="1" max="11" bind:value />
-	<label for={id}>{label}: {value}</label>
+	<label for={id}>{label}</label><span class="value">{value}</span>
 </div>
+
+<style>
+	.slider {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+		margin: 1rem 0;
+	}
+
+	.value {
+		margin: 0 0.25rem;
+		padding: 0.25rem;
+		background-color: var(--color-gray-100);
+		border-radius: 0.2rem;
+	}
+</style>
