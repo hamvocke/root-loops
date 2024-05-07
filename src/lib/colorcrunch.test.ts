67b5fd01f7936da2129c4bf7715e692a14216a33
Ham Vocke <ham@hamvocke.com>
2024-05-07 08:27:31 +0200

    only test through the 'crunch()' function

--- src/lib/colorcrunch.test.ts
+++ src/lib/colorcrunch.test.ts
@@ -1,74 +1,13 @@
 import Color from "colorjs.io";
 import { describe, expect, it } from "vitest";
-import { crunch, equalHueDistance, equalLightnessDistance, normalizeChroma } from "./colorcrunch";
+import { crunch, normalizeChroma } from "./colorcrunch";
 
-describe("equalHueDistance", () => {
-  it("generates the right amount of colors", () => {
-    const colors = equalHueDistance(16);
-    expect(colors.length).toBe(16);
-  });
-
+describe("crunch()", () => {
   it("generates colors in oklch space", () => {
-    const colors = equalHueDistance(5);
-    expect(colors.every((c) => c.space === Color.spaces.oklch)).toBeTruthy();
+    const cereals = crunch();
+    expect(Object.values(cereals).every((c) => c.space === Color.spaces.oklch)).toBeTruthy();
   });
 
-  it("generates colors with equal hue distance", () => {
-    const colors = equalHueDistance(8, 0.6, 0.2, 15);
-
-    expect(colors[0].h).toBe(15);
-    expect(colors[1].h).toBe(60);
-    expect(colors[2].h).toBe(105);
-    expect(colors[3].h).toBe(150);
-    expect(colors[4].h).toBe(195);
-    expect(colors[5].h).toBe(240);
-    expect(colors[6].h).toBe(285);
-    expect(colors[7].h).toBe(330);
-  });
-
-  it("generates colors with given lightness and chroma values", () => {
-    const colors = equalHueDistance(1, 0.2, 0.1);
-
-    expect(colors[0].l).toBe(0.2);
-    expect(colors[0].c).toBe(0.1);
-  });
-
-  it("generates bowl colors with given chroma value", () => {
-    const colors = equalHueDistance(1, 0.2);
-
-    expect(colors[0].c).toBe(0.2);
-  });
-});
-
-describe("equalLightnessDistance", () => {
-  it("generates bowl colors in oklch space", () => {
-    const colors = equalLightnessDistance(5);
-    expect(colors.every((c) => c.space === Color.spaces.oklch)).toBeTruthy();
-  });
-
-  it("generates the right amount of colors", () => {
-    const colors = equalLightnessDistance(16);
-    expect(colors.length).toBe(16);
-  });
-
-  it("generates colors with equal lightness distance", () => {
-    const colors = equalLightnessDistance(4);
-
-    expect(colors[0].l).toBe(0);
-    expect(colors[1].l).toBe(0.33);
-    expect(colors[2].l).toBe(0.67);
-    expect(colors[3].l).toBe(1);
-  });
-
-  it("generates colors with given chroma values", () => {
-    const colors = equalLightnessDistance(2, 0.2);
-
-    expect(colors[0].c).toBe(0.2);
-    expect(colors[1].c).toBe(0.2);
-  });
-});
-
-describe("crunch", () => {
   it("generates base tone cereals with low chroma", () => {
     const cereals = crunch();
 
--- src/lib/colorcrunch.ts
+++ src/lib/colorcrunch.ts
@@ -1,34 +1,34 @@
 import Color from "colorjs.io";
 
-export function equalHueDistance(
+function equalHueDistance(
   count: number,
   l: number = 0.6,
   c: number = 0.2,
   shift: number = 0,
 ): Color[] {
+  function hue(index: number, colorCount: number, shift: number = 0): number {
+    const maxHue = 360;
+    const step = maxHue / colorCount;
+    return Math.round(step * index) + shift;
+  }
+
   const h = (i: number) => hue(i, count, shift);
 
   return new Array(count).fill(undefined).map((_color, i) => new Color("oklch", [l, c, h(i)]));
 }
 
-export function equalLightnessDistance(count: number, chroma: number = 0.02): Color[] {
+function equalLightnessDistance(count: number, chroma: number = 0.02): Color[] {
+  function lightness(index: number, colorCount: number): number {
+    const maxLightness = 100;
+    const step = maxLightness / (colorCount - 1);
+    return Math.round(step * index) / 100;
+  }
+
   const l = (i: number) => lightness(i, count);
 
   return new Array(count)
     .fill(undefined)
     .map((_color, i) => new Color("oklch", [l(i), chroma, 340]));
-}
-
-function hue(index: number, colorCount: number, shift: number = 0): number {
-  const maxHue = 360;
-  const step = maxHue / colorCount;
-  return Math.round(step * index) + shift;
-}
-
-function lightness(index: number, colorCount: number): number {
-  const maxLightness = 100;
-  const step = maxLightness / (colorCount - 1);
-  return Math.round(step * index) / 100;
 }
 
 export type CrunchOptions = {
@@ -61,7 +61,8 @@ export type Cereals = {
 // TODO: more mixers?
 // milk -> drives brightness of base colors
 // sugar -> drives brightness of cereals (accents)
-// artificial colors -> drives chroma of accents, slightly drives chroma of base
+// artificial colors -> drives chroma of accents
+// sogginess -> drives chroma of base colors
 // flavor -> drives hue shift of accent colors
 // liquid -> determines base hue of base colors (orange juice, water, milk, grape juice, energy drink)
 export function crunch(options?: CrunchOptions): Cereals {
