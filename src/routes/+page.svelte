c92a448ef2b4bfdc77fadcf7c2ef06bd61fa65f7
Ham Vocke <ham@hamvocke.com>
2024-05-15 09:50:38 +0200

    move snippets to extra file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Slider from "./Slider.svelte";
   import Code from "./Code.svelte";
   import { prepare } from "$lib/cereals";
+  import { javascriptSnippet, pythonSnippet, ansiSnippet } from "$lib/snippets";
   import "@fontsource/luckiest-guy";
 
   let milk = 3;
@@ -62,59 +63,6 @@
 --root-loops-ansi-bright-white: ${cereals.brightWhite};
 --root-loops-ansi-bright-white-dim: ${cereals.brightWhite};
 `;
-
-  const javascriptSnippet = `async function transfer(fromAccount: string, toAccount: string, amount: number) {
-	// These are activities, not regular functions.
-	// Activities may run elsewhere, and their return value
-	// is automatically persisted by Temporal.
-	await myActivities.withdraw(fromAccount, amount);
-	try {
-		await myActivities.deposit(toAccount, amount);
-	} catch {
-		await myActivities.deposit(fromAccount, amount);
-	}
-}`;
-  const pythonSnippet = `@activity.defn
-async def compose_greeting(input: ComposeGreetingInput) -> str:
-    print(f"Invoking activity, attempt number {activity.info().attempt}")
-    # Fail the first 3 attempts, succeed the 4th
-    if activity.info().attempt < 4:
-        raise RuntimeError("Intentional failure" )
-    return f"{input.greeting}, {input.name}!"
-
-
-@workflow.defn
-class GreetingWorkflow:
-    @workflow.run
-    async def run(self, name: str) -> str:
-        # By default activities will retry, backing off an initial interval and
-        # then using a coefficient of 2 to increase the backoff each time after
-        # for an unlimited amount of time and an unlimited number of attempts.
-        # We'll keep those defaults except we'll set the maximum interval to
-        # just 2 seconds.
-        return await workflow.execute_activity(
-            compose_greeting,
-            ComposeGreetingInput("Hello", name),
-            start_to_close_timeout =timedelta(seconds=10),
-            retry_policy=RetryPolicy(maximum_interval=timedelta(seconds=2)),
-        )`;
-
-  const ansiSnippet = `\u001b[0;90m┌\u001b[0m  \u001b[0;36;1mWelcome to VitePress!\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;32m◇\u001b[0m  Where should VitePress initialize the config?\u001b[0m
-\u001b[0;90m│\u001b[0m  \u001b[0;2m./docs\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;32m◇\u001b[0m  Site title:\u001b[0m
-\u001b[0;90m│\u001b[0m  \u001b[0;2mMy Awesome Project\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;32m◇\u001b[0m  Site description:\u001b[0m
-\u001b[0;90m│\u001b[0m  \u001b[0;2mA VitePress Site\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;36m◆\u001b[0m  Theme:\u001b[0m
-\u001b[0;36m│\u001b[0m  \u001b[0;32m●\u001b[0m Default Theme \u001b[0;2m(Out of the box, good-looking docs)\u001b[0m\u001b[0m
-\u001b[0;36m│\u001b[0m  \u001b[0;2m○\u001b[0m \u001b[0;2mDefault Theme + Customization\u001b[0m\u001b[0m
-\u001b[0;36m│\u001b[0m  \u001b[0;2m○\u001b[0m \u001b[0;2mCustom Theme\u001b[0m\u001b[0m
-\u001b[0;36m└\u001b[0m`;
 </script>
 
 <svelte:head>
