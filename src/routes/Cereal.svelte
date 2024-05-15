2b4765ec40172d95ae1aa395f3f11f47e3a3ed76
Ham Vocke <ham@hamvocke.com>
2024-05-15 14:05:40 +0200

    tweak colors, outline, hover style of cereals

--- src/routes/Cereal.svelte
+++ src/routes/Cereal.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <div class="wrapper">
-  <div class="cereal" style="--color: {color}"></div>
+  <div class="cereal" style="--color: {color}">
+    <div class="hole"></div>
+  </div>
   <span class="color-value">{color}</span>
   {#if !color.inGamut("srgb")}<span title="Not in srgb gamut">⚠️</span>{/if}
 </div>
@@ -23,7 +25,25 @@
     width: clamp(3rem, 9vw, 128px);
     border-radius: 50%;
     background: var(--color);
-    border: 0.25rem solid #fffa;
+    border: 3px solid color-mix(in oklch, var(--color) 90%, black);
+    outline: 0.4rem solid color-mix(in oklch, var(--color) 10%, transparent);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.1s ease-out;
+
+    &:hover {
+      transform: scale(1.2);
+      outline-width: 0.8rem;
+    }
+  }
+
+  .hole {
+    height: clamp(1rem, 3vw, 32px);
+    width: clamp(1rem, 3vw, 32px);
+    border-radius: 50%;
+    background: white;
+    opacity: 75%;
   }
 
   .color-value {
