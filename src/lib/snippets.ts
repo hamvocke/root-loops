ec0b722888919236c104f88ae1e324e9a98181c8
Ham Vocke <ham@hamvocke.com>
2024-05-22 12:30:53 +0200

    update snippets

--- src/lib/snippets.ts
+++ src/lib/snippets.ts
@@ -1,56 +1,172 @@
-export const javascript = `async function transfer(fromAccount: string, toAccount: string, amount: number) {
-	// These are activities, not regular functions.
-	// Activities may run elsewhere, and their return value
-	// is automatically persisted by Temporal.
-	await myActivities.withdraw(fromAccount, amount);
-	try {
-		await myActivities.deposit(toAccount, amount);
-	} catch {
-		await myActivities.deposit(fromAccount, amount);
-	}
-}`;
+export const typescript = `enum LogLevel {
+  ERROR,
+  WARN,
+  INFO,
+  DEBUG,
+}
 
-export const python = `@activity.defn
-async def compose_greeting(input: ComposeGreetingInput) -> str:
-    print(f"Invoking activity, attempt number {activity.info().attempt}")
-    # Fail the first 3 attempts, succeed the 4th
-    if activity.info().attempt < 4:
-        raise RuntimeError("Intentional failure" )
-    return f"{input.greeting}, {input.name}!"
+/**
+ * This is equivalent to:
+ * type LogLevelStrings = 'ERROR' | 'WARN' | 'INFO' | 'DEBUG';
+ */
+type LogLevelStrings = keyof typeof LogLevel;
+
+function printImportant(key: LogLevelStrings, message: string) {
+  const num = LogLevel[key];
+  if (num <= LogLevel.WARN) {
+    console.log("Log level key is:", key);
+    console.log("Log level value is:", num);
+    console.log("Log level message is:", message);
+  }
+}
+printImportant("ERROR", "This is a message");`;
+
+const morevitest = `
+ FAIL  src/lib/cereals.test.ts > prepare(defaultRecipe) > should always fail
+AssertionError: expected true to be false // Object.is equality
+
+- Expected
++ Received
+
+- false
++ true
+
+ ❯ src/lib/cereals.test.ts:246:49
+    244|   });
+    245|
+    246|   it("should always fail", () => { expect(true).toBe(false); });
+       |                                                 ^
+    247|   it.todo("should be marked as todo");
+    248| });
+
+⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[1/1]⎯
+`;
+
+export const python = `def fib_iterative(n: int) -> list[int]:
+    """
+    Calculates the first n (0-indexed) Fibonacci numbers using iteration
+    >>> fib_iterative(0)
+    [0]
+    >>> fib_iterative(1)
+    [0, 1]
+    >>> fib_iterative(5)
+    [0, 1, 1, 2, 3, 5]
+    >>> fib_iterative(10)
+    [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    >>> fib_iterative(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: n is negative
+    """
+    if n < 0:
+        raise ValueError("n is negative")
+    if n == 0:
+        return [0]
+    fib = [0, 1]
+    for _ in range(n - 1):
+        fib.append(fib[-1] + fib[-2]) # fib that thing
+    return fib
+
+# via https://github.com/TheAlgorithms/Python/blob/master/maths/fibonacci.py`;
+
+export const elixir = `defmodule Algorithms.Sorting.QuickSort do
+  @moduledoc """
+  Implementation of QuickSort algorithm (https://en.wikipedia.org/wiki/Quicksort)
+
+  You will be given an array of numbers, you have to sort numbers in ascending order
+  using quick sort algorithm.
+
+  Quick sort algorithm uses a divide and conquer strategy.
+  It takes a pivot, and sort other numbers in functions of the pivot, divided them
+  in a smaller and a bigger group. Execute this step until there is empty list.
+
+  Example:
+   [2, 1, 5, 4, 3]
+    1. pivot:2 smaller:[1] bigger:[5, 4, 3]
+    2. 2 [1] [5, 4, 3]
+    3. 2 [1] |
+             | 5 [4, 3] []
+    4. 2 [1] |
+               | 5 |
+                   | 4 [3] []
+    5. [1, 2, 3, 4, 5] # Recursion reunite all splited lists.
+
+   [2, 1, 5, 4, 3] => [1, 2, 3, 4, 5]
+  Complexity: O(n log n)
+  """
+  require Integer
+
+  @doc """
+    take a List and return the List with the numbers ordered
+  """
+  @spec quick_sort(list(Integer)) :: list(Integer)
+  def quick_sort([]), do: []
+
+  def quick_sort([head | tail]) do
+    smaller = Enum.filter(tail, fn x -> x <= head end) |> quick_sort()
+    bigger = Enum.filter(tail, fn x -> x > head end) |> quick_sort()
+
+    Enum.concat(smaller, [head]) |> Enum.concat(bigger)
+  end
+end`;
+
+export const moreVitest = `[0m[32mloops@cereal[0m:[0m[34m~/dev/root-loops[0m$ npm run test:unit
+
+[7m[1m[36m RUN [39m[22m[27m [36mv1.5.0[39m [90m/home/ham/dev/root-loops[39m[0m
 
 
-@workflow.defn
-class GreetingWorkflow:
-    @workflow.run
-    async def run(self, name: str) -> str:
-        # By default activities will retry, backing off an initial interval and
-        # then using a coefficient of 2 to increase the backoff each time after
-        # for an unlimited amount of time and an unlimited number of attempts.
-        # We'll keep those defaults except we'll set the maximum interval to
-        # just 2 seconds.
-        return await workflow.execute_activity(
-            compose_greeting,
-            ComposeGreetingInput("Hello", name),
-            start_to_close_timeout =timedelta(seconds=10),
-            retry_policy=RetryPolicy(maximum_interval=timedelta(seconds=2)),
-        )`;
+ [32m✓[39m src/lib/colors.test.ts (20)
+ [33m❯[39m src/lib/cereals.test.ts (21)
+   [33m❯[39m prepare(defaultRecipe) (21)
+     [32m✓[39m generates colors in oklch space
+     [32m✓[39m generates base tone cereals with low chroma
+     [32m✓[39m generates red tone cereals with right hue
+     [32m✓[39m generates yellow tone cereals with right hue
+     [32m✓[39m generates green tone cereals with right hue
+     [32m✓[39m generates cyan tone cereals with right hue
+     [32m✓[39m generates blue tone cereals with right hue
+     [32m✓[39m generates magenta tone cereals with right hue
+     [32m✓[39m generates bright colors with higher lightness than regular colors
+     [32m✓[39m creates dark base colors for milk amount of 'None'
+     [32m✓[39m creates lighter base colors for milk amount of 'Splash'
+     [32m✓[39m creates light base colors for milk amount of 'Glug'
+     [32m✓[39m creates lightest base colors for milk amount of 'Cup'
+     [32m✓[39m ignores 'milk' parameter for accent colors
+     [32m✓[39m uses 'artificial colors' parameter to drive chroma of accent colors
+     [32m✓[39m uses 'flavor' parameter to drive hue shift of accent colors
+     [32m✓[39m uses 'sugar' parameter to drive lightness of accent colors
+     [32m✓[39m uses 'juice' parameter to drive hue of base colors
+     [32m✓[39m uses 'sogginess' parameter to drive chroma of base colors
+     [31m×[39m should always fail
+     [90m↓[39m should be marked as todo [90m[skipped][39m
 
-export const ansi = `\u001b[0;90m┌\u001b[0m  \u001b[0;36;1mWelcome to VitePress!\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;32m◇\u001b[0m  Where should VitePress initialize the config?\u001b[0m
-\u001b[0;90m│\u001b[0m  \u001b[0;2m./docs\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;32m◇\u001b[0m  Site title:\u001b[0m
-\u001b[0;90m│\u001b[0m  \u001b[0;2mMy Awesome Project\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;32m◇\u001b[0m  Site description:\u001b[0m
-\u001b[0;90m│\u001b[0m  \u001b[0;2mA VitePress Site\u001b[0m\u001b[0m
-\u001b[0;90m│\u001b[0m\u001b[0m
-\u001b[0;36m◆\u001b[0m  Theme:\u001b[0m
-\u001b[0;36m│\u001b[0m  \u001b[0;32m●\u001b[0m Default Theme \u001b[0;2m(Out of the box, good-looking docs)\u001b[0m\u001b[0m
-\u001b[0;36m│\u001b[0m  \u001b[0;2m○\u001b[0m \u001b[0;2mDefault Theme + Customization\u001b[0m\u001b[0m
-\u001b[0;36m│\u001b[0m  \u001b[0;2m○\u001b[0m \u001b[0;2mCustom Theme\u001b[0m\u001b[0m
-\u001b[0;36m└\u001b[0m`;
+[31m⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ [7m[1m[31m Failed Tests 1 [39m[22m[27m[31m ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[39m[0m
+
+
+[7m[1m[31m FAIL [39m[22m[27m[0m  src/lib/cereals.test.ts > prepare(defaultRecipe) > should always fail
+[1m[31mAssertionError:[0m [31mexpected true to be false // Object.is equality[0m
+
+[32m- Expected[0m
+[31m+ Received[0m
+
+[32m- false[0m
+[31m+ true[0m
+
+ [36m❯ src/lib/cereals.test.ts:246:49[0m
+    [90m244|[39m   });
+    [90m245|[39m
+    [90m246|[39m   [34mit[39m([32m"should always fail"[39m, () [33m=>[39m { [34mexpect[39m([35mtrue[39m).[34mtoBe[39m([35mfalse[39m); });
+       [90m|[39m                                                 ^
+    [90m247|[39m   it.todo("should be marked as todo");
+    [90m248|[39m });
+
+[31m⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[1/1]⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯[39m
+
+ Test Files  [31m1 failed[39m | [32m1 passed[39m [90m(2)[39m
+      Tests  [31m1 failed[39m | [32m39 passed[39m | [90m1 todo (41)[39m
+   Start at  12:06:05
+   Duration  337ms (transform 87ms, setup 0ms, collect 135ms, tests 30ms, environment 0ms, prepare 133ms)
+`;
 
 export const vitest = `[7m[1m[36m RUN [39m[22m[27m [36mv1.5.0[39m [90m/home/ham/dev/root-loops[39m
 
