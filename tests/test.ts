4d7e21156ba8fef08194ca02298e1402aaae5ad6
Ham Vocke <ham@hamvocke.com>
2024-05-22 20:00:18 +0200

    add new sliders to playwright test

--- tests/test.ts
+++ tests/test.ts
@@ -9,5 +9,8 @@ test("index page shows all range sliders", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByRole("slider", { name: "Milk" })).toBeVisible();
   await expect(page.getByRole("slider", { name: "Flavor" })).toBeVisible();
+  await expect(page.getByRole("slider", { name: "Sugar" })).toBeVisible();
   await expect(page.getByRole("slider", { name: "Artificial Colors" })).toBeVisible();
+  await expect(page.getByRole("slider", { name: "Juice" })).toBeVisible();
+  await expect(page.getByRole("slider", { name: "Sogginess" })).toBeVisible();
 });
