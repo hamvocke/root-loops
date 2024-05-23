8f43ad4d550ceabda58b987c44f594885287fb80
Ham Vocke <ham@hamvocke.com>
2024-05-23 14:37:23 +0200

    group playwright tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,53 +1,53 @@
 import { expect, test } from "@playwright/test";
 
-test("index page shows heading", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByRole("heading", { name: "Root Loops", exact: true })).toBeVisible();
-});
+test.describe("index", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
 
-test("index page shows all range sliders", async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByRole("slider", { name: "Milk" })).toBeVisible();
-  await expect(page.getByRole("slider", { name: "Flavor" })).toBeVisible();
-  await expect(page.getByRole("slider", { name: "Sugar" })).toBeVisible();
-  await expect(page.getByRole("slider", { name: "Artificial Colors" })).toBeVisible();
-  await expect(page.getByRole("slider", { name: "Juice" })).toBeVisible();
-  await expect(page.getByRole("slider", { name: "Sogginess" })).toBeVisible();
-});
+  test("shows heading", async ({ page }) => {
+    await expect(page.getByRole("heading", { name: "Root Loops", exact: true })).toBeVisible();
+  });
 
-test("index page has bowl with 16 cereals", async ({ page }) => {
-  await page.goto("/");
+  test("shows all range sliders", async ({ page }) => {
+    await expect(page.getByRole("slider", { name: "Milk" })).toBeVisible();
+    await expect(page.getByRole("slider", { name: "Flavor" })).toBeVisible();
+    await expect(page.getByRole("slider", { name: "Sugar" })).toBeVisible();
+    await expect(page.getByRole("slider", { name: "Artificial Colors" })).toBeVisible();
+    await expect(page.getByRole("slider", { name: "Juice" })).toBeVisible();
+    await expect(page.getByRole("slider", { name: "Sogginess" })).toBeVisible();
+  });
 
-  await expect(page.getByRole("button", { name: "black", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "red", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "green", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "yellow", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "blue", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "magenta", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "cyan", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "white", exact: true })).toBeVisible();
+  test("has bowl with 16 cereals", async ({ page }) => {
+    await expect(page.getByRole("button", { name: "black", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "red", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "green", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "yellow", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "blue", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "magenta", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "cyan", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "white", exact: true })).toBeVisible();
 
-  await expect(page.getByRole("button", { name: "bright black", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "bright red", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "bright green", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "bright yellow", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "bright blue", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "bright magenta", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "bright cyan", exact: true })).toBeVisible();
-  await expect(page.getByRole("button", { name: "bright white", exact: true })).toBeVisible();
-});
+    await expect(page.getByRole("button", { name: "bright black", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "bright red", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "bright green", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "bright yellow", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "bright blue", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "bright magenta", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "bright cyan", exact: true })).toBeVisible();
+    await expect(page.getByRole("button", { name: "bright white", exact: true })).toBeVisible();
+  });
 
-test("index page has terminal with different tabs", async ({ page }) => {
-  await page.goto("/");
+  test("has terminal with different tabs", async ({ page }) => {
+    const terminal = page.getByRole("region", { name: "Terminal Preview", exact: true });
+    await expect(terminal).toBeVisible();
 
-  const terminal = page.getByRole("region", { name: "Terminal Preview", exact: true });
-  await expect(terminal).toBeVisible();
+    await expect(terminal.getByRole("tab")).toHaveCount(5);
 
-  await expect(terminal.getByRole("tab")).toHaveCount(5);
+    await terminal.getByRole("tab").filter({ hasText: "vitest" }).click();
 
-  await terminal.getByRole("tab").filter({ hasText: "vitest" }).click();
-
-  await expect(terminal.getByRole("tabpanel")).toContainText(
-    "test.js > root loops color generator > pours milk into the bowl",
-  );
+    await expect(terminal.getByRole("tabpanel")).toContainText(
+      "test.js > root loops color generator > pours milk into the bowl",
+    );
+  });
 });
