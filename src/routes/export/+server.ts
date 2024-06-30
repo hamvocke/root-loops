4aa87bde126b02ef6cd955b83f3ae5ecf71168c8
Ham Vocke <ham@hamvocke.com>
2024-06-30 18:22:55 +0200

    add 'format' parameter to export page

--- src/routes/export/+server.ts
+++ src/routes/export/+server.ts
@@ -1,10 +1,33 @@
 import { fromQueryString } from "$lib/ingredients";
-import { exportToJson } from "$lib/export";
+import { exportToAlacritty, exportToJson } from "$lib/export";
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = ({ url }) => {
   const recipe = fromQueryString(url.searchParams);
-  const config = exportToJson(recipe);
+  const format = url.searchParams.get("format") ?? "json";
 
-  return new Response(String(config));
+  switch (format) {
+    case "json":
+      return jsonResponse(exportToJson(recipe));
+    case "alacritty":
+      return textResponse(exportToAlacritty(recipe));
+  }
+
+  return jsonResponse(exportToJson(recipe));
 };
+
+function textResponse(body: string): Response {
+  return new Response(body, {
+    status: 200,
+    statusText: "OK",
+    headers: { "Content-Type": "text/plain" },
+  });
+}
+
+function jsonResponse(body: string): Response {
+  return new Response(body, {
+    status: 200,
+    statusText: "OK",
+    headers: { "Content-Type": "application/json" },
+  });
+}
--- tests/export.test.ts
+++ tests/export.test.ts
@@ -1,12 +1,44 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("export", () => {
-  test("can export json format", async ({ page }) => {
+  test("exports json format by default", async ({ page }) => {
     await page.goto("/");
 
+    const responsePromise = page.waitForResponse(
+      "**/export?format=json&sugar=7&colors=6&sogginess=4&flavor=1&fruit=10&milk=0",
+    );
     const exportLink = page.getByRole("link", { name: "Export" });
     await exportLink.click();
 
-    await page.waitForURL("**/export?sugar=7&colors=6&sogginess=4&flavor=1&fruit=10&milk=0");
+    const expectedResponseBody = {
+      source: "rootloops.sh",
+      hex: {
+        background: "#0a0611",
+        foreground: "#b2a2d1",
+        text: "#0a0611",
+        cursor: "#f2eff8",
+        black: "#0a0611",
+        red: "#d97780",
+        green: "#7aa860",
+        yellow: "#bc904f",
+        blue: "#6b9bd9",
+        magenta: "#b77ed1",
+        cyan: "#52a9a9",
+        white: "#b2a2d1",
+        brightBlack: "#584875",
+        brightRed: "#e6949a",
+        brightGreen: "#8ebf73",
+        brightYellow: "#d3a563",
+        brightBlue: "#88b1e5",
+        brightMagenta: "#c899de",
+        brightCyan: "#63c0bf",
+        brightWhite: "#f2eff8",
+      },
+    };
+
+    const response = await responsePromise;
+
+    expect(await response.headerValue("Content-Type")).toBe("application/json");
+    expect(await response.json()).toEqual(expectedResponseBody);
   });
 });
