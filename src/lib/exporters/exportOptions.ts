485a883e9c92c065264e82477a76217a3ecd8b09
Ham Vocke <ham@hamvocke.com>
2024-10-03 17:43:18 +0200

    sort export options alphabetically

--- src/lib/exporters/exportOptions.ts
+++ src/lib/exporters/exportOptions.ts
@@ -2,22 +2,22 @@ import { type SelectOption } from "$lib/selectOptions";
 
 export enum ExportFormat {
   JSON = 0,
-  WindowsTerminal = 1,
-  Alacritty = 2,
-  XResources = 3,
-  Kitty = 4,
-  WezTerm = 5,
-  // Neovim = 6,
-  Helix = 7,
+  WindowsTerminal,
+  Alacritty,
+  XResources,
+  Kitty,
+  WezTerm,
+  // Neovim,
+  Helix,
 }
 
 export const exportSelectOptions: SelectOption[] = [
   { value: ExportFormat.JSON, label: "JSON", group: "General" },
-  { value: ExportFormat.WindowsTerminal, label: "Windows Terminal", group: "Terminal Emulators" },
   { value: ExportFormat.Alacritty, label: "Alacritty", group: "Terminal Emulators" },
-  { value: ExportFormat.XResources, label: "XResources", group: "Terminal Emulators" },
   { value: ExportFormat.Kitty, label: "Kitty", group: "Terminal Emulators" },
   { value: ExportFormat.WezTerm, label: "WezTerm", group: "Terminal Emulators" },
+  { value: ExportFormat.WindowsTerminal, label: "Windows Terminal", group: "Terminal Emulators" },
+  { value: ExportFormat.XResources, label: "XResources", group: "Terminal Emulators" },
   // { value: ExportFormat.Neovim, label: "Neovim", group: "Editors" },
   { value: ExportFormat.Helix, label: "Helix", group: "Editors" },
 ];
