e409c785e0b892a34a72ff4c347822271355677c
Ham Vocke <ham@hamvocke.com>
2024-10-06 15:20:38 +0200

    apparently 'kitty' prefers lowercase spelling

--- src/lib/export/options.ts
+++ src/lib/export/options.ts
@@ -10,7 +10,7 @@ export const exportSelectOptions: ExportOption[] = [
   { value: "Alacritty", label: "Alacritty", group: "Terminal Emulators" },
   { value: "Foot", label: "foot", group: "Terminal Emulators" },
   { value: "Ghostty", label: "ghostty", group: "Terminal Emulators" },
-  { value: "Kitty", label: "Kitty", group: "Terminal Emulators" },
+  { value: "Kitty", label: "kitty", group: "Terminal Emulators" },
   { value: "WezTerm", label: "WezTerm", group: "Terminal Emulators" },
   { value: "WindowsTerminal", label: "Windows Terminal", group: "Terminal Emulators" },
   { value: "Xresources", label: "Xresources", group: "Terminal Emulators" },
