5c6b1783338e30bed473362b9570538a4cb2ef33
Ham Vocke <ham@hamvocke.com>
2024-10-12 11:06:30 +0200

    switch 'colortest' output to use bright colors, not bold
    closes #15

--- snippets/colortest.sh
+++ snippets/colortest.sh
@@ -4,12 +4,12 @@
 
 T='gYw' # The test text
 
-echo -e "\n                 40m     41m     42m     43m\
+echo -e "               40m     41m     42m     43m\
      44m     45m     46m     47m";
 
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-           '  36m' '1;36m' '  37m' '1;37m';
+for FGs in '  m' ' 1m' '30m' '90m' '31m' '91m' '32m' \
+           '92m' '33m' '93m' '34m' '94m' '35m' '95m' \
+           '36m' '96m' '37m' '97m';
   do FG=${FGs// /}
   echo -en " $FGs \033[$FG  $T  "
   for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
@@ -17,4 +17,3 @@ for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
   done
   echo;
 done
-echo
