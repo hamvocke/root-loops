2181ce1e55b4acb1004f224d9106056e699e0bc0
Ham Vocke <ham@hamvocke.com>
2024-12-02 20:31:31 +0100

    use different favicon in dev mode

--- src/lib/favicon.ts
+++ src/lib/favicon.ts
@@ -1,6 +1,23 @@
 import { type Cereals } from "$lib/cereals";
+import { dev } from "$app/environment";
 
 function favicon(cereals: Cereals) {
+  let colors = {
+    firstFill: cereals.red.color_hex,
+    firstStroke: cereals.brightRed.color_hex,
+    secondFill: cereals.blue.color_hex,
+    secondStroke: cereals.brightBlue.color_hex,
+  };
+
+  if (dev) {
+    colors = {
+      firstFill: cereals.magenta.color_hex,
+      firstStroke: cereals.brightMagenta.color_hex,
+      secondFill: cereals.cyan.color_hex,
+      secondStroke: cereals.brightCyan.color_hex,
+    };
+  }
+
   return `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 180 180'>
     <mask id='hole-red'>
       <rect width='100%' height='100%' fill='white'/>
@@ -11,10 +28,10 @@ function favicon(cereals: Cereals) {
       <rect width='100%' height='100%' fill='white'/>
       <circle r='20' cx='110' cy='110' fill='black'/>
     </mask>
-    <circle cx='110' cy='110' r='60' fill='${cereals.blue.color_hex}' stroke='${cereals.brightBlue.color_hex}' stroke-width='10' mask='url(#hole-blue)' />
-    <circle cx='110' cy='110' r='30' fill='${cereals.brightBlue.color_hex}' mask='url(#hole-blue)' />
-    <circle cx='65' cy='65' r='60' fill='${cereals.red.color_hex}' stroke='${cereals.brightRed.color_hex}' stroke-width='10' mask='url(#hole-red)'/>
-    <circle cx='65' cy='65' r='30' fill='${cereals.brightRed.color_hex}' mask='url(#hole-red)'/>
+    <circle cx='110' cy='110' r='60' fill='${colors.secondFill}' stroke='${colors.secondStroke}' stroke-width='10' mask='url(#hole-blue)' />
+    <circle cx='110' cy='110' r='30' fill='${colors.secondStroke}' mask='url(#hole-blue)' />
+    <circle cx='65' cy='65' r='60' fill='${colors.firstFill}' stroke='${colors.firstStroke}' stroke-width='10' mask='url(#hole-red)'/>
+    <circle cx='65' cy='65' r='30' fill='${colors.firstStroke}' mask='url(#hole-red)'/>
   </svg>`.replaceAll("#", "%23");
 }
 
