4619f3c148ad91fb4b1b96145c395a80a55773b4
Ham Vocke <ham@hamvocke.com>
2024-12-04 20:31:08 +0100

    remove outdated todo

--- src/lib/export/formats.ts
+++ src/lib/export/formats.ts
@@ -3,6 +3,7 @@ import { toFoot } from "./exporters/foot";
 import { toFzf } from "./exporters/fzf";
 import { toGhostty } from "./exporters/ghostty";
 import { toHelix } from "./exporters/helix";
+import { toITerm } from "./exporters/iterm";
 import { toJson } from "./exporters/json";
 import { toKitty } from "./exporters/kitty";
 import { toWezTerm } from "./exporters/wezterm";
@@ -13,9 +14,10 @@ import { toZellij } from "./exporters/zellij";
 export const exporters = {
   Alacritty: { export: toAlacritty },
   Foot: { export: toFoot },
-  fzf: { export: toFzf }, // TODO: not yet exposed, need to figure out better gray shades first
+  fzf: { export: toFzf },
   Ghostty: { export: toGhostty },
   Helix: { export: toHelix },
+  iTerm: { export: toITerm },
   JSON: { export: toJson },
   Kitty: { export: toKitty },
   WezTerm: { export: toWezTerm },
