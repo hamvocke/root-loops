01a66428d615a16a9ff4bf0d07da41c6eac18b78
Henré Botha <henrebotha@gmail.com>
2025-01-28 12:23:04 +0100

    Improve types

--- src/lib/export/exporters/neovim.ts
+++ src/lib/export/exporters/neovim.ts
@@ -5,32 +5,26 @@ import { prepare, type Cereals } from "$lib/cereals";
  * with a lot of gratitute to https://github.com/romainl/vim-rnb
  */
 
-type HighlightGroup = [
-  name: string,
-  background: ColorDefinition | "NONE" | "fg" | "bg",
-  foreground: ColorDefinition | "NONE" | "fg" | "bg",
-  style: "NONE" | "bold" | "underline" | "reverse" | "italic" | "standout" | "undercurl" | string,
-];
+type HighlightGroup = {
+  name: string;
+  background: ColorDefinition | "NONE" | "fg" | "bg";
+  foreground: ColorDefinition | "NONE" | "fg" | "bg";
+  style: "NONE" | "bold" | "underline" | "reverse" | "italic" | "standout" | "undercurl" | string;
+  undercurl?: ColorDefinition;
+};
 
-// TODO: remove this type, add undercurl as optional field to highlightgroup
-type UndercurlHighlightGroup = [
-  name: string,
-  background: ColorDefinition | "NONE" | "fg" | "bg",
-  foreground: ColorDefinition | "NONE" | "fg" | "bg",
-  style: "NONE" | "bold" | "underline" | "reverse" | "italic" | "standout" | "undercurl" | string,
-  undercurl: ColorDefinition,
-];
+type LinkedHighlightGroup = {
+  highlight1: string;
+  highlight2: string;
+};
 
-// TODO: define Links separately to avoid union type?
-type LinkedHighlightGroup = [string, string];
+type HighlightGroups = Array<HighlightGroup | LinkedHighlightGroup>;
 
-type HighlightGroups = Array<HighlightGroup | UndercurlHighlightGroup | LinkedHighlightGroup>;
-
-type ColorDefinition = [
-  hex: string, // used for true-color terminals
-  octal: number | "NONE", // used for 256 color terminals
-  name:  // used for less capable terminals
-    | "black"
+type ColorDefinition = {
+  hex: string; // used for true-color terminals
+  octal: number | "NONE"; // used for 256 color terminals
+  name: // used for less capable terminals
+  | "black"
     | "darkred"
     | "darkgreen"
     | "darkyellow"
@@ -46,116 +40,156 @@ type ColorDefinition = [
     | "magenta"
     | "cyan"
     | "white"
-    | "NONE",
-];
+    | "NONE";
+};
 
 function generateHighlights(cereals: Cereals): HighlightGroups {
-  const background: ColorDefinition = [cereals.background.color_hex, 0, "black"];
-  const foreground: ColorDefinition = [cereals.foreground.color_hex, 15, "white"];
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  const background: ColorDefinition = {
+    hex: cereals.background.color_hex,
+    octal: 0,
+    name: "black",
+  };
+  const foreground: ColorDefinition = {
+    hex: cereals.foreground.color_hex,
+    octal: 15,
+    name: "white",
+  };
 
-  const black: ColorDefinition = [cereals.black.color_hex, 0, "black"];
-  const darkred: ColorDefinition = [cereals.red.color_hex, 1, "darkred"];
-  const darkgreen: ColorDefinition = [cereals.green.color_hex, 2, "darkgreen"];
-  const darkyellow: ColorDefinition = [cereals.yellow.color_hex, 3, "darkyellow"];
-  const darkblue: ColorDefinition = [cereals.blue.color_hex, 4, "darkblue"];
-  const darkmagenta: ColorDefinition = [cereals.magenta.color_hex, 5, "darkmagenta"];
-  const darkcyan: ColorDefinition = [cereals.cyan.color_hex, 6, "darkcyan"];
-  const gray: ColorDefinition = [cereals.white.color_hex, 7, "gray"];
+  const black: ColorDefinition = { hex: cereals.black.color_hex, octal: 0, name: "black" };
+  const darkred: ColorDefinition = { hex: cereals.red.color_hex, octal: 1, name: "darkred" };
+  const darkgreen: ColorDefinition = { hex: cereals.green.color_hex, octal: 2, name: "darkgreen" };
+  const darkyellow: ColorDefinition = {
+    hex: cereals.yellow.color_hex,
+    octal: 3,
+    name: "darkyellow",
+  };
+  const darkblue: ColorDefinition = { hex: cereals.blue.color_hex, octal: 4, name: "darkblue" };
+  const darkmagenta: ColorDefinition = {
+    hex: cereals.magenta.color_hex,
+    octal: 5,
+    name: "darkmagenta",
+  };
 
-  const darkgray: ColorDefinition = [cereals.brightBlack.color_hex, 8, "darkgray"];
-  const red: ColorDefinition = [cereals.brightRed.color_hex, 9, "red"];
-  const green: ColorDefinition = [cereals.brightGreen.color_hex, 10, "green"];
-  const yellow: ColorDefinition = [cereals.brightYellow.color_hex, 11, "yellow"];
-  const blue: ColorDefinition = [cereals.brightBlue.color_hex, 12, "blue"];
-  const magenta: ColorDefinition = [cereals.brightMagenta.color_hex, 13, "magenta"];
-  const cyan: ColorDefinition = [cereals.brightCyan.color_hex, 14, "cyan"];
-  const white: ColorDefinition = [cereals.brightWhite.color_hex, 15, "white"];
+  const darkcyan: ColorDefinition = { hex: cereals.cyan.color_hex, octal: 6, name: "darkcyan" };
+  const gray: ColorDefinition = { hex: cereals.white.color_hex, octal: 7, name: "gray" };
+
+  const darkgray: ColorDefinition = {
+    hex: cereals.brightBlack.color_hex,
+    octal: 8,
+    name: "darkgray",
+  };
+  const red: ColorDefinition = { hex: cereals.brightRed.color_hex, octal: 9, name: "red" };
+  const green: ColorDefinition = { hex: cereals.brightGreen.color_hex, octal: 10, name: "green" };
+  const yellow: ColorDefinition = {
+    hex: cereals.brightYellow.color_hex,
+    octal: 11,
+    name: "yellow",
+  };
+  const blue: ColorDefinition = { hex: cereals.brightBlue.color_hex, octal: 12, name: "blue" };
+  const magenta: ColorDefinition = {
+    hex: cereals.brightMagenta.color_hex,
+    octal: 13,
+    name: "magenta",
+  };
+  const cyan: ColorDefinition = { hex: cereals.brightCyan.color_hex, octal: 14, name: "cyan" };
+  const white: ColorDefinition = { hex: cereals.brightWhite.color_hex, octal: 15, name: "white" };
+  /* eslint-enable @typescript-eslint/no-unused-vars */
 
   // Highlight groups
   // see :help highlight-default and :help group-name for additional groups
-  return [
-    ["Normal", background, foreground, "NONE"],
-    ["NonText", background, white, "NONE"],
-    ["EndOfBuffer", "NonText"],
-    ["Comment", background, darkgray, "italic"],
-    ["Constant", background, darkyellow, "NONE"],
-    ["Error", darkred, black, "NONE"],
-    ["Identifier", background, darkblue, "NONE"],
-    ["Ignore", background, foreground, "NONE"],
-    ["PreProc", background, foreground, "NONE"],
-    ["Special", background, darkcyan, "NONE"],
-    ["Statement", background, darkred, "NONE"],
-    ["String", background, darkgreen, "NONE"],
-    ["Number", "Constant"],
-    ["Todo", white, darkgray, "NONE"],
-    ["Type", white, darkgray, "NONE"],
-    ["Underlined", white, darkgray, "NONE"],
-    ["StatusLine", white, darkgray, "NONE"],
-    ["StatusLineNC", white, darkgray, "NONE"],
-    ["StatusLineTerm", "StatusLine"],
-    ["StatusLineTermNC", "StatusLineNC"],
-    ["VertSplit", darkgray, black, "NONE"],
-    ["TabLine", white, darkgray, "NONE"],
-    ["TabLineFill", white, darkgray, "NONE"],
-    ["TabLineSel", white, darkgray, "NONE"],
-    ["Title", white, darkgray, "NONE"],
-    ["CursorLine", darkgray, white, "NONE"],
-    ["LineNr", black, darkgray, "NONE"],
-    ["CursorLineNr", white, darkgray, "NONE"],
-    ["helpLeadBlank", white, darkgray, "NONE"],
-    ["helpNormal", white, darkgray, "NONE"],
-    ["Visual", white, darkgray, "NONE"],
-    ["VisualNOS", white, darkgray, "NONE"],
-    ["Pmenu", white, darkgray, "NONE"],
-    ["PmenuSbar", white, darkgray, "NONE"],
-    ["PmenuSel", white, darkgray, "NONE"],
-    ["PmenuThumb", white, darkgray, "NONE"],
-    ["FoldColumn", white, darkgray, "NONE"],
-    ["Folded", white, darkgray, "NONE"],
-    ["WildMenu", black, white, "NONE"],
-    ["SpecialKey", white, darkgray, "NONE"],
-    ["DiffAdd", white, darkgray, "NONE"],
-    ["DiffChange", white, darkgray, "NONE"],
-    ["DiffDelete", white, darkgray, "NONE"],
-    ["DiffText", white, darkgray, "NONE"],
-    ["IncSearch", white, darkgray, "NONE"],
-    ["Search", white, darkgray, "NONE"],
-    ["Directory", white, darkgray, "NONE"],
-    ["MatchParen", white, darkgray, "NONE"],
-    ["SpellBad", white, darkgray, "NONE", red],
-    ["SpellCap", white, darkgray, "NONE", blue],
-    ["SpellLocal", white, darkgray, "NONE", magenta],
-    ["SpellRare", white, darkgray, "NONE", cyan],
-    ["ColorColumn", white, darkgray, "NONE"],
-    ["SignColumn", white, darkgray, "NONE"],
-    ["ErrorMsg", white, darkgray, "NONE"],
-    ["ModeMsg", white, darkgray, "NONE"],
-    ["MoreMsg", white, darkgray, "NONE"],
-    ["Question", white, darkgray, "NONE"],
-    ["WarningMsg", "Error"],
-    ["Cursor", white, darkgray, "NONE"],
-    ["CursorIM", "Cursor"],
-    ["CursorColumn", white, darkgray, "NONE"],
-    ["QuickFixLine", white, darkgray, "NONE"],
-    ["Terminal", "Normal"],
-    ["Conceal", white, darkgray, "NONE"],
-    ["ToolbarLine", white, darkgray, "NONE"],
-    ["ToolbarButton", white, darkgray, "NONE"],
-    ["debugPC", white, darkgray, "NONE"],
-    ["debugBreakpoint", white, darkgray, "NONE"],
+  const highlights: HighlightGroups = [
+    { name: "Normal", background, foreground, style: "NONE" },
+    { name: "NonText", background, foreground: white, style: "NONE" },
+    { highlight1: "EndOfBuffer", highlight2: "NonText" },
+    { name: "Comment", background, foreground: darkgray, style: "italic" },
+    { name: "Constant", background, foreground: darkyellow, style: "NONE" },
+    { name: "Error", background: darkred, foreground: black, style: "NONE" },
+    { name: "Identifier", background, foreground: darkblue, style: "NONE" },
+    { name: "Ignore", background, foreground, style: "NONE" },
+    { name: "PreProc", background, foreground, style: "NONE" },
+    { name: "Special", background, foreground: darkcyan, style: "NONE" },
+    { name: "Statement", background, foreground: darkred, style: "NONE" },
+    { name: "String", background, foreground: darkgreen, style: "NONE" },
+    { highlight1: "Number", highlight2: "Constant" },
+    { name: "Todo", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Type", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Underlined", background: white, foreground: darkgray, style: "NONE" },
+    { name: "StatusLine", background: white, foreground: darkgray, style: "NONE" },
+    { name: "StatusLineNC", background: white, foreground: darkgray, style: "NONE" },
+    { highlight1: "StatusLineTerm", highlight2: "StatusLine" },
+    { highlight1: "StatusLineTermNC", highlight2: "StatusLineNC" },
+    { name: "VertSplit", background: darkgray, foreground: black, style: "NONE" },
+    { name: "TabLine", background: white, foreground: darkgray, style: "NONE" },
+    { name: "TabLineFill", background: white, foreground: darkgray, style: "NONE" },
+    { name: "TabLineSel", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Title", background: white, foreground: darkgray, style: "NONE" },
+    { name: "CursorLine", background: darkgray, foreground: white, style: "NONE" },
+    { name: "LineNr", background: black, foreground: darkgray, style: "NONE" },
+    { name: "CursorLineNr", background: white, foreground: darkgray, style: "NONE" },
+    { name: "helpLeadBlank", background: white, foreground: darkgray, style: "NONE" },
+    { name: "helpNormal", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Visual", background: white, foreground: darkgray, style: "NONE" },
+    { name: "VisualNOS", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Pmenu", background: white, foreground: darkgray, style: "NONE" },
+    { name: "PmenuSbar", background: white, foreground: darkgray, style: "NONE" },
+    { name: "PmenuSel", background: white, foreground: darkgray, style: "NONE" },
+    { name: "PmenuThumb", background: white, foreground: darkgray, style: "NONE" },
+    { name: "FoldColumn", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Folded", background: white, foreground: darkgray, style: "NONE" },
+    { name: "WildMenu", background: black, foreground: white, style: "NONE" },
+    { name: "SpecialKey", background: white, foreground: darkgray, style: "NONE" },
+    { name: "DiffAdd", background: white, foreground: darkgray, style: "NONE" },
+    { name: "DiffChange", background: white, foreground: darkgray, style: "NONE" },
+    { name: "DiffDelete", background: white, foreground: darkgray, style: "NONE" },
+    { name: "DiffText", background: white, foreground: darkgray, style: "NONE" },
+    { name: "IncSearch", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Search", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Directory", background: white, foreground: darkgray, style: "NONE" },
+    { name: "MatchParen", background: white, foreground: darkgray, style: "NONE" },
+    { name: "SpellBad", background: white, foreground: darkgray, style: "NONE", undercurl: red },
+    { name: "SpellCap", background: white, foreground: darkgray, style: "NONE", undercurl: blue },
+    {
+      name: "SpellLocal",
+      background: white,
+      foreground: darkgray,
+      style: "NONE",
+      undercurl: magenta,
+    },
+    { name: "SpellRare", background: white, foreground: darkgray, style: "NONE", undercurl: cyan },
+    { name: "ColorColumn", background: white, foreground: darkgray, style: "NONE" },
+    { name: "SignColumn", background: white, foreground: darkgray, style: "NONE" },
+    { name: "ErrorMsg", background: white, foreground: darkgray, style: "NONE" },
+    { name: "ModeMsg", background: white, foreground: darkgray, style: "NONE" },
+    { name: "MoreMsg", background: white, foreground: darkgray, style: "NONE" },
+    { name: "Question", background: white, foreground: darkgray, style: "NONE" },
+    { highlight1: "WarningMsg", highlight2: "Error" },
+    { name: "Cursor", background: white, foreground: darkgray, style: "NONE" },
+    { highlight1: "CursorIM", highlight2: "Cursor" },
+    { name: "CursorColumn", background: white, foreground: darkgray, style: "NONE" },
+    { name: "QuickFixLine", background: white, foreground: darkgray, style: "NONE" },
+    { highlight1: "Terminal", highlight2: "Normal" },
+    { name: "Conceal", background: white, foreground: darkgray, style: "NONE" },
+    { name: "ToolbarLine", background: white, foreground: darkgray, style: "NONE" },
+    { name: "ToolbarButton", background: white, foreground: darkgray, style: "NONE" },
+    { name: "debugPC", background: white, foreground: darkgray, style: "NONE" },
+    { name: "debugBreakpoint", background: white, foreground: darkgray, style: "NONE" },
   ];
+
+  return highlights;
 }
 
 function renderHighlights(highlights: HighlightGroups): string {
   const renderedHighlights = [];
   for (const highlight of highlights) {
-    if (highlight.length === 4) {
-      // TODO: use types instead of array indexing notation
-      const hi = `hi ${highlight[0]} ctermbg=${typeof highlight[1] === "string" ? highlight[1] : highlight[1][1]} ctermfg=${typeof highlight[2] === "string" ? highlight[2] : highlight[2][1]} cterm=${highlight[3]} guibg=${typeof highlight[1] === "string" ? highlight[1] : highlight[1][0]} guifg=${typeof highlight[2] === "string" ? highlight[2] : highlight[2][0]} gui=${highlight[3]}`;
-      renderedHighlights.push(hi);
-    } else if (highlight.length > 4) {
-      const hi = `hi ${highlight[0]} ctermbg=${typeof highlight[1] === "string" ? highlight[1] : highlight[1][1]} ctermfg=${typeof highlight[2] === "string" ? highlight[2] : highlight[2][1]} cterm=${highlight[3]} guibg=${typeof highlight[1] === "string" ? highlight[1] : highlight[1][0]} guifg=${typeof highlight[2] === "string" ? highlight[2] : highlight[2][0]} gui=${highlight[3]} guisp=${typeof highlight[4] === "string" ? highlight[4] : highlight[4][0]}`;
-      renderedHighlights.push(hi);
+    if ("name" in highlight) {
+      if (highlight.undercurl) {
+        const hi = `hi ${highlight.name} ctermbg=${typeof highlight.background === "string" ? highlight.background : highlight.background.octal} ctermfg=${typeof highlight.foreground === "string" ? highlight.foreground : highlight.foreground.octal} cterm=${highlight.style} guibg=${typeof highlight.background === "string" ? highlight.background : highlight.background.hex} guifg=${typeof highlight.foreground === "string" ? highlight.foreground : highlight.foreground.hex} gui=${highlight.style} guisp=${typeof highlight.undercurl === "string" ? highlight.undercurl : highlight.undercurl.hex}`;
+        renderedHighlights.push(hi);
+      } else {
+        const hi = `hi ${highlight.name} ctermbg=${typeof highlight.background === "string" ? highlight.background : highlight.background.octal} ctermfg=${typeof highlight.foreground === "string" ? highlight.foreground : highlight.foreground.octal} cterm=${highlight.style} guibg=${typeof highlight.background === "string" ? highlight.background : highlight.background.hex} guifg=${typeof highlight.foreground === "string" ? highlight.foreground : highlight.foreground.hex} gui=${highlight.style}`;
+        renderedHighlights.push(hi);
+      }
     }
   }
 
@@ -166,9 +200,8 @@ function renderHighlights(highlights: HighlightGroups): string {
 function renderSimpleHighlights(highlights: HighlightGroups): string {
   const renderedHighlights = [];
   for (const highlight of highlights) {
-    // TODO: use types instead of array indexing notation - should be a check if this is not a link
-    if (highlight.length > 2) {
-      const hi = `hi ${highlight[0]} ctermbg=${typeof highlight[1] === "string" ? highlight[1] : highlight[1][2]} ctermfg=${typeof highlight[2] === "string" ? highlight[2] : highlight[2][2]} cterm=${highlight[3]}`;
+    if ("name" in highlight) {
+      const hi = `hi ${highlight.name} ctermbg=${typeof highlight.background === "string" ? highlight.background : highlight.background.name} ctermfg=${typeof highlight.foreground === "string" ? highlight.foreground : highlight.foreground.name} cterm=${highlight.style}`;
       renderedHighlights.push(hi);
     }
   }
@@ -180,8 +213,8 @@ function renderSimpleHighlights(highlights: HighlightGroups): string {
 function renderLinks(links: HighlightGroups): string {
   const renderedLinks = [];
   for (const link of links) {
-    if (link.length === 2) {
-      renderedLinks.push(`hi link ${link[0]} ${link[1]}`);
+    if ("highlight1" in link) {
+      renderedLinks.push(`hi link ${link.highlight1} ${link.highlight2}`);
     }
   }
 
