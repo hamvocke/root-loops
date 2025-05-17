e2a53918a5115e4828cb668525788eb6a2584da5
Osama Qarem <osamaqarem@gmail.com>
2025-05-17 21:47:10 +0200

    Add Warp export format

---
+++ src/lib/export/exporters/warp.ts
@@ -0,0 +1,38 @@
+import { type Recipe } from "$lib/ingredients";
+import { prepare } from "$lib/cereals";
+
+export function toWarp(recipe: Recipe): string {
+  const cereals = prepare(recipe);
+
+  return `# Copy the configuration below and add it to a new file under
+# ~/.warp/themes/rootloops.yaml
+# Open Warp's Settings > Appearance > Themes and select the 'Root Loops' theme.
+
+# via rootloops.sh
+
+name: Root Loops
+accent: '${cereals.white.color_hex}'
+cursor: '${cereals.white.color_hex}'
+background: '${cereals.background.color_hex}'
+foreground: '${cereals.foreground.color_hex}'
+details: darker
+terminal_colors:
+  bright:
+    black: '${cereals.brightBlack.color_hex}'
+    blue: '${cereals.brightBlue.color_hex}'
+    cyan: '${cereals.brightCyan.color_hex}'
+    green: '${cereals.brightGreen.color_hex}'
+    magenta: '${cereals.brightMagenta.color_hex}'
+    red: '${cereals.brightRed.color_hex}'
+    white: '${cereals.brightWhite.color_hex}'
+    yellow: '${cereals.brightYellow.color_hex}'
+  normal:
+    black: '${cereals.black.color_hex}'
+    blue: '${cereals.blue.color_hex}'
+    cyan: '${cereals.cyan.color_hex}'
+    green: '${cereals.green.color_hex}'
+    magenta: '${cereals.magenta.color_hex}'
+    red: '${cereals.red.color_hex}'
+    white: '${cereals.white.color_hex}'
+    yellow: '${cereals.yellow.color_hex}'`;
+}
