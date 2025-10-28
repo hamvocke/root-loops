39695d40ff288d8262e93af5e765dc78557ac947
Ham Vocke <ham@hamvocke.com>
2025-10-28 08:07:21 +0100

    update vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,28 @@ import { svelteTesting } from "@testing-library/svelte/vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [sveltekit(), svelteTesting()],
+  plugins: [sveltekit()],
   test: {
-    environment: "jsdom",
-    include: ["src/**/*.{test,spec}.{js,ts}"],
+    projects: [
+      {
+        extends: "./vite.config.ts",
+        plugins: [svelteTesting()],
+        test: {
+          name: "client",
+          environment: "jsdom",
+          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+          exclude: ["src/lib/**"],
+        },
+      },
+      {
+        extends: "./vite.config.ts",
+        test: {
+          name: "lib",
+          environment: "node",
+          include: ["src/**/*.{test,spec}.{js,ts}"],
+          exclude: ["src/**/*.svelte.{test,specc}.{js,ts}"],
+        },
+      },
+    ],
   },
 });
